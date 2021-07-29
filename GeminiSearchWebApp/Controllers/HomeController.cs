6162f49362513b1666d5fa05d57ea9f2a43dcbde
Sunil Bharti <Sunil.Bharti1@nab.com.au>
2021-07-29 16:08:46 +1000

    Base code has been created
    Code checked in with initail setup

---
+++ GeminiSearchWebApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using GeminiSearchWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeminiSearchWebApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var lstClaim = User.Claims.ToList();
+            //if(User.IsInRole("Users"))
+            //{
+            //    Content("i belongs to admin");
+            //}
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GeminiSearchWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeminiSearchWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
