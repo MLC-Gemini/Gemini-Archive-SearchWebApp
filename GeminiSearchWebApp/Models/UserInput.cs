a580b23bebcc4433a62e620dabe32a6b2bd99d82
Roshini Mohan <Roshini.X.Mohan@nab.com.au>
2021-08-05 21:09:24 +1000

    Create UserInput.cs
    UserInput.cs class

---
+++ GeminiSearchWebApp/Models/UserInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeminiSearchWebApp.Models
+{
+    public class UserInput
+    {
+        public string FilterLevel { get; set; }
+
+        public string UserId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public string CaseTypeDate { get; set; }
+    }
+}
