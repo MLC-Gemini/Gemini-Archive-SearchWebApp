5edf26f6993d87433bcbd8f46563e0c74d5bc048
Sunil Bharti <Sunil.Bharti1@nab.com.au>
2021-08-30 15:47:15 +1000

    Add files via upload

--- Batch/get_batch_artifact.sh
+++ Batch/get_batch_artifact.sh
@@ -9,20 +9,14 @@ if [ ! -d $stage_folder ]; then
     mkdir $stage_folder
 fi
 
-#echo $crms_arti_uid
-#echo $art_login_pwd
-#echo $BUILD_STAGE
+gemini_arti_uid=Srv-gemi-build-np
+gemini_login_pwd=zY3eb2XJAPbW
+
+#echo $gemini_arti_uid
+#echo $gemini_login_pwd
 
 cd $stage_folder
-#curl -C - -u $crms_arti_uid:$art_login_pwd -O "https://artifactory.ext.national.com.au/artifactory/$BUILD_STAGE/linuxx64_12201_client.zip"
-curl -C - -u AUR\Srv-gemi-build-np:zY3eb2XJAPbW -O "https://artifactory.ext.national.com.au/artifactory/GEMINI-build/nginx-1.20.1.tar.gz"
-curl -C - -u AUR\Srv-gemi-build-np:zY3eb2XJAPbW -O "https://artifactory.ext.national.com.au/artifactory/GEMINI-build/aspnetcore-runtime-5.0.9-linux-x64.tar.gz"
+curl -C - -u $gemini_arti_uid:$gemini_login_pwd -O "https://artifactory.ext.national.com.au/artifactory/GEMINI-build/nginx-1.20.1.tar.gz"
+#curl -C - -u $gemini_arti_uid:$gemini_login_pwd -O "https://artifactory.ext.national.com.au/artifactory/GEMINI-build/aspnetcore-runtime-5.0.9-linux-x64.tar.gz"
 
-if [[ "$env_id" == "$BUILD_STAGE" ]]; then
-    echo "This is non-prod download"
-    #curl -C - -u $geminiweb_arti_uid:$art_login_pwd -O "https://artifactory.ext.national.com.au/artifactory/MLCDEVOPS-build/BMC/ControlM/ControlM-Agent-install-9.0.19.100_Linux-x86_64.tar.gz"
-else
-    echo "This is prod download"
-    #curl -C - -u $geminiweb_arti_uid:$art_login_pwd -O "https://artifactory.ext.national.com.au/artifactory/MLCDEVOPS-release/BMC/ControlM/ControlM-Agent-install-9.0.19.100_Linux-x86_64.tar.gz"
-fi
 
