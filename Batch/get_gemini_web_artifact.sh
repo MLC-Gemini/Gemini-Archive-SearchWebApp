b2d8b161a8ae38cce9aaecb7e567f38e099da542
Sandip Wankhede <X033172d@aurdev.national.com.au>
2021-09-15 18:55:22 +1000

    added no_proxy in get artifactory file

--- Batch/get_gemini_web_artifact.sh
+++ Batch/get_gemini_web_artifact.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/bash
+
 env_id=$1
 stage_folder=$2
 source ./Batch/var/read_variables.sh $env_id
-#export no_proxy=$NO_PROXY
+export no_proxy=$NO_PROXY
 
 # Getting Gemini artifactory service account password form AWS SSM parameter.
 gemini_login_pwd=`aws ssm get-parameter --name $gemini_arti_ssm --with-decryption --region "ap-southeast-2" | grep Value | awk '{print $2}'|sed 's/"//g'|sed 's/,$//g'`
@@ -10,8 +12,9 @@ if [ ! -d $stage_folder ]; then
     mkdir $stage_folder
 fi
 
-#echo $gemini_arti_uid
-#echo $gemini_login_pwd
+echo $gemini_arti_uid
+echo $gemini_login_pwd
+echo $NO_PROXY
 
 cd $stage_folder
 curl -C - -u $gemini_arti_uid:$gemini_login_pwd -O "https://artifactory.ext.national.com.au/artifactory/GEMINI-build/nginx-1.20.1.tar.gz"
