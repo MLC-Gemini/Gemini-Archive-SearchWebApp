43da2b0644e7037f9683fb450421ae094300d189
Cloud User <ec2-user@ip-10-156-193-84.extnp.national.com.au>
2021-09-17 15:31:24 +1000

    Modified UserData issue

--- Batch/template/Linux_Batch_Autoscaling.sh
+++ Batch/template/Linux_Batch_Autoscaling.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/bash
 
 echo '#!/bin/bash' > tmp_batch_userdata_$$
-#env_id="nonprod"
-#source ./Batch/var/read_variables.sh $env_id
-
-# Launch config/app server does not have permission to do this
-##Update DNS Name for SMB Server - todo
-#echo 'aws route53 change-resource-record-sets --hosted-zone-id '$CRMS_DNS_ZONE_ID' --change-batch ''
-# { "Changes":
-#        [
-#
-#                {       "Action": "UPSERT",
-#                        "ResourceRecordSet": {
-#                                "Name": "'$BATCH_DNS.$CRMS_DNS_ZONE_NAME'",
-#                                "Type": "CNAME",
-#                                "TTL": 3600,
-#                                "ResourceRecords": [
-#                                        { "Value": "$(hostname -i)"}
-#                                ]
-#                        }
-#                }
-#        ]
-#}
-#' >> tmp_batch_userdata_$$
-
-#Apply patch in background when autoscaling kicks in
 echo "echo 'curl https://hip.ext.national.com.au/hip_upgrade.sh | bash -s -- -a latest' > /tmp/patch-me.sh" >> tmp_batch_userdata_$$
 echo "nohup sudo /usr/bin/bash /tmp/patch-me.sh &" >> tmp_batch_userdata_$$
 
@@ -37,7 +13,7 @@ aws cloudformation deploy \
                   "InstanceType=$InstanceType" \
                   "KeyPairName=$KeyPairName" \
                   "RemoteAccessCIDR=$RemoteAccessCIDR" \
-                  "Subnets=$Subnet1.$Subnet2.$Subnet3" \
+                  Subnets="$Subnet1,$Subnet2,$Subnet3" \
                   "VpcId=$VpcId" \
                   "ApplicationID=$ApplicationID" \
                   "Owner=$Owner" \
@@ -49,33 +25,16 @@ aws cloudformation deploy \
                   "AppCategory=$AppCategory" \
                   "SupportGroup=$SupportGroup" \
                   "PowerMgt=$PowerMgt"
-        #         "IAMInstanceProfile=$IAM_PROFILE_INST" \
-        #         "ImageId=`aws ssm get-parameter --name "/gemini_archive_web/ami_image-Deploy" --with-decryption --region "ap-southeast-2" | grep Value | awk '{print $2}'|sed 's/"//g'|sed 's/,$//g'"` \
-        #         "InstanceType=$INSTANCE_TYPE_BATCH" \
-        #         "KeyPairName=$KEYPAIR_NAME" \
-        #         "RemoteAccessCIDR=$SSHACCESSCIDR" \
-        #         "Subnets=$SUBNETID1,$SUBNETID2,$SUBNETID3" \
-        #         "VpcId=$VPCID" \
-        #         "ApplicationID=$T_ApplicationID" \
-        #         "Owner=$Owner" \
-        #         "UserData1=$(cat tmp_batch_userdata_$$|openssl base64 -A)" \
-        # --tags \
-        #         "CostCentre=$T_CostCentre" \
-        #         "Name=GeminiWeb-$T_Environment" \
-        #         "Environment=$T_Environment" \
-        #         "AppCategory=$T_AppCategory" \
-        #         "SupportGroup=$T_SupportGroup" \
-        #         "PowerMgt=$T_EC2_PowerMgt"
 cat tmp_batch_userdata_$$
 rm tmp_batch_userdata_$$
 
 #restart all EC2 by killing them.
-# asg_group_name=$(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$Name| jq -r '.StackResources[]|select (.LogicalResourceId=="AutoScalingGroup").PhysicalResourceId'|sed 's/ /,/g')
-# instances=$(aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances[]|select (.AutoScalingGroupName=="'$asg_group_name'").InstanceId'|paste -sd " " -)
-# aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --instance-id  \
-#                 $(aws autoscaling describe-auto-scaling-groups  --auto-scaling-group-names  \
-#                         $(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$Name \
-#                         |jq -r '.StackResources[]|select (.ResourceType=="AWS::AutoScaling::AutoScalingGroup").PhysicalResourceId')  \
-#                 |jq -r '.AutoScalingGroups[0].Instances[].InstanceId') \
-#         |jq -r '.Reservations[].Instances[].InstanceId')
+ asg_group_name=$(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$Name| jq -r '.StackResources[]|select (.LogicalResourceId=="AutoScalingGroup").PhysicalResourceId'|sed 's/ /,/g')
+ instances=$(aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances[]|select (.AutoScalingGroupName=="'$asg_group_name'").InstanceId'|paste -sd " " -)
+ aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --instance-id  \
+                 $(aws autoscaling describe-auto-scaling-groups  --auto-scaling-group-names  \
+                         $(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$Name \
+                         |jq -r '.StackResources[]|select (.ResourceType=="AWS::AutoScaling::AutoScalingGroup").PhysicalResourceId')  \
+                 |jq -r '.AutoScalingGroups[0].Instances[].InstanceId') \
+         |jq -r '.Reservations[].Instances[].InstanceId')
 
--- deploy_batch_server.sh
+++ deploy_batch_server.sh
@@ -101,19 +101,19 @@ bash tmp_launch_asg_$$.sh
 
 # ######################################
 # echo "3. Set DNS entry"
-# lb_arn=$(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$T_Environment \
-# 	|jq -r '.StackResources[]|select (.LogicalResourceId=="LoadBalancer").PhysicalResourceId')
-# lb_dns=$(aws elbv2 describe-load-balancers --load-balancer-arns $lb_arn |jq -r '.LoadBalancers[0].DNSName')
-# #./aws/aws_set_dns.sh $dbname $BATCH_DNS.$CRMS_DNS_ZONE_NAME $lb_dns
+ lb_arn=$(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$T_Environment \
+ 	|jq -r '.StackResources[]|select (.LogicalResourceId=="LoadBalancer").PhysicalResourceId')
+ lb_dns=$(aws elbv2 describe-load-balancers --load-balancer-arns $lb_arn |jq -r '.LoadBalancers[0].DNSName')
+ ./aws/aws_set_dns.sh $dbname $BATCH_DNS.$CRMS_DNS_ZONE_NAME $lb_dns
 
 # ##Added below code as CAST requirement to verify the resource is up and running
-# while [[   $(aws ec2 describe-instances --instance-id  \
-#                 $(aws autoscaling describe-auto-scaling-groups  --auto-scaling-group-names  \
-#                         $(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$T_Environment \
-#                         |jq -r '.StackResources[]|select (.ResourceType=="AWS::AutoScaling::AutoScalingGroup").PhysicalResourceId')  \
-#                 |jq -r '.AutoScalingGroups[0].Instances[].InstanceId') \
-#         | jq -r '.Reservations[].Instances[].State.Name') != 'running' ]]
-# do
-# 	echo "Wait for ec2 instance ready......"
-# 	sleep 60
-# done
+ while [[   $(aws ec2 describe-instances --instance-id  \
+                 $(aws autoscaling describe-auto-scaling-groups  --auto-scaling-group-names  \
+                         $(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$T_Environment \
+                         |jq -r '.StackResources[]|select (.ResourceType=="AWS::AutoScaling::AutoScalingGroup").PhysicalResourceId')  \
+                 |jq -r '.AutoScalingGroups[0].Instances[].InstanceId') \
+         | jq -r '.Reservations[].Instances[].State.Name') != 'running' ]]
+ do
+ 	echo "Wait for ec2 instance ready......"
+ 	sleep 60
+ done
