224b41aadc4a568c014c4f565f4a222205fe33e6
Cloud User <ec2-user@ip-10-156-193-238.extnp.national.com.au>
2021-09-20 15:53:28 +1000

    Delete groovy modificaiton

--- _pipeline/destroy_gemini_web_server.sh
+++ _pipeline/destroy_gemini_web_server.sh
@@ -4,7 +4,9 @@ env_id="nonprod"
 #env_id=$1
 
 #Must resume ASG first, otherwise, delete will fail 
-aws autoscaling resume-processes --auto-scaling-group-name $(aws cloudformation describe-stacks --stack-name GEMINI-WEB-$env_id | jq -r '.Stacks[].Outputs[]|select (.OutputKey=="AutoScalingGroup").OutputValue')
+#aws autoscaling resume-processes --auto-scaling-group-name $(aws cloudformation describe-stacks --stack-name GEMINI-WEB-$env_id | jq -r '.Stacks[].Outputs[]|select (.OutputKey=="AutoScalingGroup").OutputValue')
 
-aws cloudformation delete-stack --stack-name GEMINI-WEB-$env_id
-aws cloudformation wait stack-delete-complete --stack-name GEMINI-WEB-$env_id
+#aws cloudformation delete-stack --stack-name GEMINI-WEB-$env_id
+#aws cloudformation wait stack-delete-complete --stack-name GEMINI-WEB-$env_id
+
+echo "We will be proceeding to delete the stack in real time"
