a9ab165d1f7e0d016bd1352b08f33f5942de1246
Sandip Wankhede <X033172d@aurdev.national.com.au>
2021-09-20 15:36:33 +1000

    Added delete and rename deploy Jenkins pipeline sh file name

--- Batch/Reference/Deploy/aws/aws_get_db_end_point.sh
+++
@@ -1,4 +0,0 @@
-#aws/aws_get_db_end_point.sh CRMSD01 Address
-#aws/aws_get_db_end_point.sh CRMSD01 Port
-#aws rds describe-db-instances |jq -r '.DBInstances[]|select(.DBName=="'$1'").Endpoint.'$2
-aws rds describe-db-instances --db-instance-identifier $1|jq -r '.DBInstances[0].Endpoint.'$2
--- Batch/Reference/Deploy/aws/checkout_release.sh
+++
@@ -1,9 +0,0 @@
-#aws/checkout_release.sh git@github.aus.thenational.com:CRMS/release.git DCExit /tmp/release/2315
-release_url=$1
-release_id=$2
-tmp_release_folder=$3
-
-rm -rf $tmp_release_folder
-mkdir -p $tmp_release_folder
-cd $tmp_release_folder
-git clone $release_url
