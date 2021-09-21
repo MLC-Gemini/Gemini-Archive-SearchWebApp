890d6652966dcd4783b56aa28f8c60cd7020415a
Sandip Wankhede <X033172d@aurdev.national.com.au>
2021-09-21 13:11:10 +1000

    Reverted https cloudformation

--- _pipeline/deploy_gemini_web_server.sh
+++ _pipeline/deploy_gemini_web_server.sh
@@ -104,7 +104,7 @@ bash tmp_launch_asg_$$.sh
  lb_arn=$(aws cloudformation describe-stack-resources --stack-name GEMINI-WEB-$T_Environment \
  	|jq -r '.StackResources[]|select (.LogicalResourceId=="LoadBalancer").PhysicalResourceId')
  lb_dns=$(aws elbv2 describe-load-balancers --load-balancer-arns $lb_arn |jq -r '.LoadBalancers[0].DNSName')
- ./aws/aws_set_dns.sh $dbname $BATCH_DNS.$CRMS_DNS_ZONE_NAME $lb_dns
+# ./aws/aws_set_dns.sh $dbname $BATCH_DNS.$CRMS_DNS_ZONE_NAME $lb_dns
 
 # ##Added below code as CAST requirement to verify the resource is up and running
  while [[   $(aws ec2 describe-instances --instance-id  \
