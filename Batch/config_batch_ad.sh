996e64fa3554456565ed73f9026ab87fa0184800
Cloud User <ec2-user@ip-10-156-193-174.extnp.national.com.au>
2021-12-01 00:13:13 +1100

    Ad groups

--- Batch/config_batch_ad.sh
+++ Batch/config_batch_ad.sh
@@ -8,8 +8,11 @@ child_domain=$2
 
 ##creating 'ad_auth_group' template variable which will contain the list of all group DNs separated by '|'
 echo "%$parent_domain-$child_domain-Delegated-SRVGEMBATCH-SudoRoot ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/10_ldapadmin
+echo "%$parent_domain-$child_domain-Delegated-SRVGEMBATCH-SudoRoot ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/01_service_accounts
 echo "%$parent_domain-$child_domain-Delegated-SRVGEMBATCH-LogonAccess ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/20_svc_acct
+echo "%$parent_domain-$child_domain-Delegated-SRVGEMBATCH-LogonAccess ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/01_service_accounts
 echo "%$parent_domain-$child_domain-Delegated-SRVGEMBATCH-SudoSrv-gem-m ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/20_svc_acct
+echo "%$parent_domain-$child_domain-Delegated-SRVGEMBATCH-SudoSrv-gem-m ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/01_service_accounts
 
 #append all group DN with '|' as separator and add as variable to /etc/facter/facts.d/ldap_auth_group.yaml
 echo "ldap_auth_group: CN=$parent_domain-$child_domain-Delegated-SRVGEMBATCH-SudoRoot,OU=Application,OU=$parent_domain,OU=Delegated,OU=Support Groups,OU=Production,DC=$child_domain,DC=national,DC=com,DC=au|CN=$parent_domain-$child_domain-Delegated-SRVGEMBATCH-LogonAccess,OU=Application,OU=$parent_domain,OU=Delegated,OU=Support Groups,OU=Production,DC=$child_domain,DC=national,DC=com,DC=au|CN=$parent_domain-$child_domain-Delegated-SRVGEMBATCH-SudoSrv-gem-m,OU=Application,OU=$parent_domain,OU=Delegated,OU=Support Groups,OU=Production,DC=$child_domain,DC=national,DC=com,DC=au" > /etc/facter/facts.d/ldap_auth_group.yaml
