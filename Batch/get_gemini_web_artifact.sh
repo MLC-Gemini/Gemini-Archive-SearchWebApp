b434a19459603f0958cc2346ea7e83033a3a7914
axv016 <143767632+axv016@users.noreply.github.com>
2024-04-03 11:41:18 +1100

    Update get_gemini_web_artifact.sh

--- Batch/get_gemini_web_artifact.sh
+++ Batch/get_gemini_web_artifact.sh
@@ -4,9 +4,9 @@ env_id=$1
 stage_folder=$2
 source ./Batch/var/read_variables.sh $env_id
 
-export http_proxy=http://forwardproxy:3128
-export https_proxy=http://forwardproxy:3128
-export no_proxy=localhost,169.254.169.254,hip.ext.national.com.au,github.aus.thenational.com,artifactory.ext.national.com.au
+#export http_proxy=http://forwardproxy:3128
+#export https_proxy=http://forwardproxy:3128
+#export no_proxy=localhost,169.254.169.254,hip.ext.national.com.au,github.aus.thenational.com,artifactory.ext.national.com.au
 #export no_proxy=$NO_PROXY
 
 # Getting Gemini artifactory service account password form AWS SSM parameter.
