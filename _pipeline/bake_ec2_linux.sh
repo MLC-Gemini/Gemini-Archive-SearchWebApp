fdc9e302038026372d2489f1405c69894175160c
Robert Davis <robert.davis@insigniafinancial.com.au>
2024-05-06 18:28:59 +1000

    temporary block nexus

--- _pipeline/bake_ec2_linux.sh
+++ _pipeline/bake_ec2_linux.sh
@@ -24,8 +24,7 @@ source ./Batch/var/read_variables.sh $env_id
 ts=`date +%Y-%m-%d-%H-%M-%S`
 
 echo "1. Download from artifactory"
-sudo cp nginx-1.25.3.tar.gz /tmp/gemini_web_staging/nginx-1.25.3.tar.gz
-#cd /tmp/gemini_web_staging
+#./Batch/get_gemini_web_artifact.sh $env_id /tmp/gemini_web_staging
 
 echo "2. Run instance using latest golden image in Baking VPC"
 ami_id=$(aws ssm get-parameter  --name "/golden-ami/rhel8/latest" --query "Parameter.Value" --output text)
