672f47ae96b67f507fd75fa160a6558ea78ebaad
axv016 <143767632+axv016@users.noreply.github.com>
2024-06-04 15:00:40 +1000

    Update ec2_install_software.sh

--- Batch/ec2_install_software.sh
+++ Batch/ec2_install_software.sh
@@ -5,10 +5,10 @@ sudo luseradd webapp -d /opt/webapp
 echo "webapp         ALL=(ALL)       NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
 
 # Directory for Gemini web application
-sudo mkdir -p /var/www
-sudo mkdir /var/www/geminiweb
-sudo chmod -R 773 /var/www/geminiweb/
-cd /var/www/geminiweb
+#sudo mkdir -p /opt/webapp
+sudo mkdir /opt/webapp/geminiweb
+sudo chmod -R 773 /opt/webapp/geminiweb/
+cd /opt/webapp/geminiweb
 
 # Copy ASP.NET core self-contained published file in web directory, change permisson and ownership and making file self-executable.
 sudo mv /tmp/Published/* .
@@ -34,8 +34,6 @@ sudo systemctl enable kestrel-geminiweb.service
 
 # Install require packages for NGINX server using "openssl-devel gcc"
 sudo yum -y install openssl-devel gcc -q -y
-#sudo yum -y install openssl-devel gcc
-#rpm -qa|egrep 'openssl-devel|pcre|zlib'
 
 # Directory for NGINX Server
 sudo mkdir -p /web/nginx
@@ -46,7 +44,6 @@ cd /web/nginx/
 sudo mv /tmp/nginx-1.20.1.tar.gz .
 #sudo tar -xzvf nginx-1.20.1.tar.gz with output below command for skipping output (-v without verbose)
 sudo tar -zxf nginx-1.20.1.tar.gz
-#ls -lrt
 
 sudo mkdir binaries 
 sudo mv nginx-1.20.1/* binaries/
@@ -55,14 +52,11 @@ sudo rm -rf nginx-1.20.1/
 sudo rm -rf nginx-1.20.1.tar.gz
 
 cd binaries/
-# pwd > /web/nginx/binaries
-# ls  -lrt
 
 sudo ./configure --prefix=/web/nginx --modules-path=/web/nginx/modules --with-http_ssl_module  --without-http_fastcgi_module --without-http_uwsgi_module --without-http_grpc_module --without-http_scgi_module --without-mail_imap_module --without-mail_pop3_module > /dev/null
 sudo make > /dev/null
 sudo make install > /dev/null
 
-# pwd > /web/nginx
 sudo rm -rf binaries/
 
 # SSl certificate 
@@ -87,9 +81,3 @@ sudo mv /tmp/nginx.service .
 sudo systemctl enable nginx.service
 sudo systemctl start nginx
 sudo systemctl status nginx
-
-#Add scheduled patching code
-crontab << EOF
-00 18 8-14 * 7 echo "START patching" >> /tmp/weekly-patching.log ; curl https://hip.ext.national.com.au/hip_upgrade.sh | bash -s -- -a latest ; echo "FINISH patching"
-00 20 8-14 * 7 echo $(date) "Restarting..." >> /tmp/weekly-patching.log ; sudo shutdown -r now
-EOF
