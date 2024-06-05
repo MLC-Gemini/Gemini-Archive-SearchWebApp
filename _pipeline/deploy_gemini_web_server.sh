8779328704ff6fcce3ef203c5215f09c25cace00
axv016 <143767632+axv016@users.noreply.github.com>
2024-06-05 15:15:34 +1000

    Update deploy_gemini_web_server.sh

--- _pipeline/deploy_gemini_web_server.sh
+++ _pipeline/deploy_gemini_web_server.sh
@@ -25,12 +25,12 @@ sed -e "s/oldAMI/$ami_id/g;s/oldSG/$ec2instanceSG/g" template/dev-rhel8.json_tem
 
 echo $ami_id
 
-aws cloudformation deploy --region ap-southeast-2 --stack-name GeminiPreDeployJune \
+aws cloudformation deploy --region ap-southeast-2 --stack-name GeminiPreDeploy \
     --template-file cloudformation/ec2-launchtemplate-key.yaml \
     --capabilities CAPABILITY_NAMED_IAM --parameter-overrides file://dev-rhel8.json
 
 instance_id=`aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId[]" \
-    --filters "Name=tag-key,Values=aws:cloudformation:stack-name" "Name=tag-value,Values=GeminiPreDeployJune" --output=text`
+    --filters "Name=tag-key,Values=aws:cloudformation:stack-name" "Name=tag-value,Values=GeminiPreDeploy" --output=text`
 
 echo "- Wait for instance status OK"
 aws ec2 wait instance-status-ok --instance-ids $instance_id
@@ -38,7 +38,7 @@ aws ec2 wait instance-status-ok --instance-ids $instance_id
 echo $instance_id
 
 ts=`date +%Y-%m-%d-%H-%M-%S`
-image_id=$(aws ec2 create-image --name Gemini-web-deploy-june-$ts --instance-id $instance_id|jq -r ".ImageId")
+image_id=$(aws ec2 create-image --name Gemini-web-deploy-$ts --instance-id $instance_id|jq -r ".ImageId")
 aws ec2 create-tags --resources $image_id --tags Key=PatchCycle,Value=$T_PatchCycle Key=Environment,Value=$T_Environment Key=T_CostCentre,Value=$T_CostCentre Key=DataClassification,Value=$T_DataClassification Key=Owner,Value=$T_Owner Key=PowerMgt,Value=$T_PowerMgt Key=Name,Value=$T_Name Key=ApplicationID,Value=$T_ApplicationID Key=OUName,Value=$T_OUName Key=map-migrated,Value=$T_MapMigrated
 
 echo "- wait for image to be created"
@@ -61,7 +61,7 @@ ec2instanceSG=$(aws ssm get-parameter  --name "/gemini_archive_web/ec2instanceSG
 
 sed -e "s/oldAMI/$ImageId/g;s/oldSG/$ec2instanceSG/g" template/dev-rhel8.json_template > dev-rhel8.json
 
-aws cloudformation deploy --region ap-southeast-2 --stack-name GEMINI-WEB-June-$env_id-Stack \
+aws cloudformation deploy --region ap-southeast-2 --stack-name GEMINI-WEB-$env_id-Stack \
     --template-file cloudformation/ec2-autoscaling-cert.yaml \
     --capabilities CAPABILITY_NAMED_IAM --parameter-overrides file://dev-rhel8.json
 	
