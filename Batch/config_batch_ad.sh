7f025bb33a968be343c46dbb96ef3e1fe818484c
axv016 <143767632+axv016@users.noreply.github.com>
2024-06-11 11:07:37 +1000

    Update config_batch_ad.sh

--- Batch/config_batch_ad.sh
+++ Batch/config_batch_ad.sh
@@ -12,7 +12,7 @@ echo "ROL_APP_Gemini ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/01_service_accou
 echo "ROL_APP_Gemini ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/20_svc_acct
 
 #append all group DN with '|' as separator and add as variable to /etc/facter/facts.d/ldap_auth_group.yaml
-echo "ldap_auth_group: CN=ROL_IFL_IT_Gemini_Sup,OU=Application,OU=$parent_domain,OU=Delegated,OU=Support Groups,OU=Production,DC=gem,DC=aws,DC=odev,DC=com,DC=au|CN=ROL_APP_Gemini,OU=Application,OU=Delegated,OU=Support Groups,OU=Production,DC=gem,DC=aws,DC=odev,DC=com,DC=au" > /etc/facter/facts.d/ldap_auth_group.yaml
+echo "ldap_auth_group: CN=ROL_IFL_IT_Gemini_Sup,OU=Application,OU=Delegated,OU=Support Groups,OU=Production,DC=gem,DC=aws,DC=odev,DC=com,DC=au|CN=ROL_APP_Gemini,OU=Application,OU=Delegated,OU=Support Groups,OU=Production,DC=gem,DC=aws,DC=odev,DC=com,DC=au" > /etc/facter/facts.d/ldap_auth_group.yaml
 
 echo 'hui_ldap_enabled: true' > /etc/facter/facts.d/hui_ldap_enabled.yaml
 echo "ldap_auth_domain: ROL" > /etc/facter/facts.d/hui_ldap_auth_domain.yaml
